862a2669528c14ec2f07cd39e606536b8a0724f6
and-89 <48600221+and-89@users.noreply.github.com>
2020-01-11 15:51:56 +0100

    Add files via upload

---
+++ bb1.ino
@@ -0,0 +1,165 @@
+//ACM CAN EMULATOR_ ACM pin 2L 6H 500MS
+//BB2
+#include <SPI.h>
+#include <mcp2515.h>
+
+#define NR_OF_MESSAGES 7
+
+class CanMessageSender
+{
+  public:
+  CanMessageSender()
+  {
+    lastTimeMessageSend = 0;
+  }
+ 
+  void set(can_frame& f, unsigned long interval)
+  {
+     frame = f;
+     intervalMs = interval;
+     lastTimeMessageSend = 0;
+  }
+
+  bool shouldBeSend(unsigned long actualTime)
+  {
+    if (actualTime > (lastTimeMessageSend + intervalMs))
+    {
+      lastTimeMessageSend = actualTime;
+      return true;
+    }
+    return false;
+  }
+ 
+  can_frame frame;
+  unsigned long intervalMs;
+  unsigned long lastTimeMessageSend;
+};
+
+
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+struct can_frame canMsg3;
+struct can_frame canMsg4;
+struct can_frame canMsg5;
+struct can_frame canMsg6;
+struct can_frame canMsg7;
+
+CanMessageSender messages[NR_OF_MESSAGES];
+
+MCP2515 mcp2515(10);
+
+
+void setup() {
+canMsg1.can_id =0x18FD7B3D | CAN_EFF_FLAG; // AT1S_X_ACM: 8, delay 145, cycle 1000
+canMsg1.can_dlc = 8;
+canMsg1.data[0] = 0x1A;   //dpf soot level
+canMsg1.data[1] = 0x1F;   //dpf ash level
+canMsg1.data[2] = 0xFF;
+canMsg1.data[3] = 0xFF;
+canMsg1.data[4] = 0xFF;
+canMsg1.data[5] = 0xFF;
+canMsg1.data[6] = 0xFF;
+canMsg1.data[7] = 0xFF;
+
+canMsg2.can_id =0x18FD7C3D | CAN_EFF_FLAG;  //2566749245 DPFC1_X_ACM: 8, delay 335, cycle 1000 
+canMsg2.can_dlc = 8;
+canMsg2.data[0] = 0x00;
+canMsg2.data[1] = 0x00;
+canMsg2.data[2] = 0x00;
+canMsg2.data[3] = 0x00;
+canMsg2.data[4] = 0x00;
+canMsg2.data[5] = 0x00;
+canMsg2.data[6] = 0x00;
+canMsg2.data[7] = 0x00;
+
+canMsg3.can_id =0x18FDB33D | CAN_EFF_FLAG;  // AT1OG2_X_ACM: 8 delay 273, cycle 500
+canMsg3.can_dlc = 8;
+canMsg3.data[0] = 0xFF;
+canMsg3.data[1] = 0xFF;
+canMsg3.data[2] = 0x9E;   //dpf exh gas temp
+canMsg3.data[3] = 0x2E;  //dpf exh gas temp
+canMsg3.data[4] = 0xFF;
+canMsg3.data[5] = 0xFF;
+canMsg3.data[6] = 0xFF;
+canMsg3.data[7] = 0xFF;
+
+canMsg4.can_id =0x18FFAA3D | CAN_EFF_FLAG;  //ACM_BB1_01P: 8, cycle 1000
+canMsg4.can_dlc = 8;
+canMsg4.data[0] = 0x2F;   //total adblue used
+canMsg4.data[1] = 0x0F;   //total adblue used
+canMsg4.data[2] = 0x03;   //total adblue used
+canMsg4.data[3] = 0x00;   //CatalystTankLevelLow 3 "Not Available" 2 "Error" 1 "Warning" 0 "No Warning" ;
+canMsg4.data[4] = 0x50;   //adblue temp 50=30deg
+canMsg4.data[5] = 0x80;   //tank level
+canMsg4.data[6] = 0xFC;
+canMsg4.data[7] = 0xFF;
+
+canMsg5.can_id =0x18ECFF3D | CAN_EFF_FLAG;   // TPCM_X_ACM Transport Protocol Connection Management, cycle 1000
+canMsg5.can_dlc = 8;
+canMsg5.data[0] = 0x20;
+canMsg5.data[1] = 0x0E;
+canMsg5.data[2] = 0x00;
+canMsg5.data[3] = 0x02;
+canMsg5.data[4] = 0xFF;
+canMsg5.data[5] = 0xCA;
+canMsg5.data[6] = 0xFE;
+canMsg5.data[7] = 0x00;
+
+canMsg6.can_id =0x18EBFF3D | CAN_EFF_FLAG;  //TPDT_X_ACM: 8, Transport Protocol Data Transfer  cycle 1000
+canMsg6.can_dlc = 8;
+canMsg6.data[0] = 0x01;
+canMsg6.data[1] = 0x00;
+canMsg6.data[2] = 0xFF;
+canMsg6.data[3] = 0x87;
+canMsg6.data[4] = 0x0E;
+canMsg6.data[5] = 0x00;
+canMsg6.data[6] = 0x09; 
+canMsg6.data[7] = 0x87;
+
+canMsg7.can_id =0x18EBFF3D | CAN_EFF_FLAG;  //TPDT_X_ACM: 8, Transport Protocol Data Transfer cycle 1000
+canMsg7.can_dlc = 8;
+canMsg7.data[0] = 0x02;
+canMsg7.data[1] = 0x0E;
+canMsg7.data[2] = 0x10;
+canMsg7.data[3] = 0x01;
+canMsg7.data[4] = 0x87;
+canMsg7.data[5] = 0x0E;
+canMsg7.data[6] = 0x0F; 
+canMsg7.data[7] = 0x01;
+
+messages[0].set(canMsg1, 1000);
+messages[1].set(canMsg2, 1000);
+messages[2].set(canMsg3, 499);
+messages[3].set(canMsg4, 1000);
+messages[5].set(canMsg2, 1000);
+messages[6].set(canMsg3, 1000);
+
+  while (!Serial);
+  Serial.begin(115200);
+  SPI.begin();
+ 
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);   //  baudrates 5,10,20,40,50,80,100,125,200,250,500,1000 clock speed MCP_20MHZ,MCP_16MHZ,MCP_8MHZ
+  mcp2515.setNormalMode(); //options: mcp2515.setNormalMode() or mcp2515.setLoopbackMode() or mcp2515.setListenOnlyMode()
+ 
+  Serial.println("Write to CAN");
+}
+
+void loop() {
+
+
+  unsigned long actualTime = millis();     //take a milliseconds from start
+
+  for (int i=0; i < NR_OF_MESSAGES; i++)
+  {
+    if (messages[i].shouldBeSend(actualTime))
+    {
+      mcp2515.sendMessage(MCP2515::TXB1, &messages[i].frame);
+      Serial.println("Message sent");
+      delay (1);
+    }
+  }
+}
+ 
+ 
+ 
---
+++ can_emu.ino
@@ -0,0 +1,125 @@
+//ACM CAN EMULATOR_ ACM pin 2L 6H 500MS
+//BB2
+#include <SPI.h>
+#include <mcp2515.h>
+
+#define NR_OF_MESSAGES 4
+
+class CanMessageSender
+{
+  public:
+  CanMessageSender()
+  {
+    lastTimeMessageSend = 0;
+  }
+ 
+  void set(can_frame& f, unsigned long interval)
+  {
+     frame = f;
+     intervalMs = interval;
+     lastTimeMessageSend = 0;
+  }
+
+  bool shouldBeSend(unsigned long actualTime)
+  {
+    if (actualTime > (lastTimeMessageSend + intervalMs))
+    {
+      lastTimeMessageSend = actualTime;
+      return true;
+    }
+    return false;
+  }
+ 
+  can_frame frame;
+  unsigned long intervalMs;
+  unsigned long lastTimeMessageSend;
+};
+
+
+struct can_frame canMsg1;
+struct can_frame canMsg2;
+struct can_frame canMsg3;
+struct can_frame canMsg4;
+
+CanMessageSender messages[NR_OF_MESSAGES];
+
+MCP2515 mcp2515(10);
+
+
+void setup() {
+canMsg1.can_id =0x1CFF903D | CAN_EFF_FLAG; // AnmMsg_ACM_Backbone2 CYCLE 100
+canMsg1.can_dlc = 4;
+canMsg1.data[0] = 0x00;
+canMsg1.data[1] = 0x00;
+canMsg1.data[2] = 0x00;
+canMsg1.data[3] = 0x00;
+
+canMsg2.can_id =0x10FF2E3D | CAN_EFF_FLAG; //CEA2P_BB2_M2_ACM: 8 CYCLE 100
+canMsg2.can_dlc = 8;
+canMsg2.data[0] = 0x00;
+canMsg2.data[1] = 0x00;
+canMsg2.data[2] = 0xFF;
+canMsg2.data[3] = 0xFF;
+canMsg2.data[4] = 0xFF;
+canMsg2.data[5] = 0xFF;
+canMsg2.data[6] = 0xFF;
+canMsg2.data[7] = 0xFF;
+
+canMsg3.can_id =0x19FF013D | CAN_EFF_FLAG; // ???? CYCLE 1000
+canMsg3.can_dlc = 8;
+canMsg3.data[0] = 0x00;
+canMsg3.data[1] = 0x00;
+canMsg3.data[2] = 0x00;
+canMsg3.data[3] = 0x00;
+canMsg3.data[4] = 0x00;
+canMsg3.data[5] = 0x00;
+canMsg3.data[6] = 0xFF;
+canMsg3.data[7] = 0x01;
+
+canMsg4.can_id =0x18F77E3D | CAN_EFF_FLAG; // CEA2P_BB2_M6_ACM CYCLE 1500
+canMsg4.can_dlc = 8;
+canMsg4.data[0] = 0x00;
+canMsg4.data[1] = 0xFE;
+canMsg4.data[2] = 0xFF;
+canMsg4.data[3] = 0xFF;
+canMsg4.data[4] = 0xFF;
+canMsg4.data[5] = 0xFF;
+canMsg4.data[6] = 0xFF;
+canMsg4.data[7] = 0xFF;
+
+messages[0].set(canMsg1, 99);
+messages[1].set(canMsg2, 99);
+messages[2].set(canMsg3, 998);
+messages[3].set(canMsg4, 1498);
+
+  while (!Serial);
+  Serial.begin(115200);
+  SPI.begin();
+ 
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);   //  baudrates 5,10,20,40,50,80,100,125,200,250,500,1000 clock speed MCP_20MHZ,MCP_16MHZ,MCP_8MHZ
+  mcp2515.setNormalMode(); //options: mcp2515.setNormalMode() or mcp2515.setLoopbackMode() or mcp2515.setListenOnlyMode()
+ 
+  Serial.println("Write to CAN");
+}
+
+void loop() {
+
+
+  unsigned long actualTime = millis();     //take a milliseconds from start
+
+  for (int i=0; i < NR_OF_MESSAGES; i++)
+  {
+    if (messages[i].shouldBeSend(actualTime))
+    {
+      mcp2515.sendMessage(MCP2515::TXB1, &messages[i].frame);
+      Serial.println("Message sent");
+      delay (1);
+    }
+  }
+ 
+ 
+ 
+
+
+}
